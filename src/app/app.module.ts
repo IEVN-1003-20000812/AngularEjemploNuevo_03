14714244b440400b15ae4a482b135ba0a20e3e13
David Luna <eusoj.divad1@gmail.com>
2023-11-03 21:04:07 -0600

    03/11/2023 JDCL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MenuComponent } from './menu/menu/menu.component';
 import { MaterialModule } from './material/material/material.module';
 import { DatosAlumnoComponent } from './formularios/datos-alumno/datos-alumno.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DatosAlumnoComponent
+    DatosAlumnoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
